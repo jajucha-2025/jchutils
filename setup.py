052fb87fdaadc0ad5188aa92cb07a7cfd9df9e82
KGH1113 <gangguhyeon1113@gmail.com>
2025-09-19 14:30:44 +0900

    init: initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jchutils",
+    version="0.0.2",
+    description="Utils for jajucha",
+    url="",
+    author="Guhyeon Kang",
+    author_email="gangguhyeon1113@gmail.com",
+    license="All rights reserved",
+    packages=find_packages(),
+    requires=[]
+)
